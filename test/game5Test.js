f9c2dbf6bba54ff84779218401b6cb90304078f1
Karim <98668332+khadni@users.noreply.github.com>
2023-02-28 13:14:50 +0100

    test5 passed

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,41 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
-    return { game };
+    const threshold = "0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf";
+
+    let Johnny, Johnny_adr;
+
+    while (true) {
+      const signer = ethers.Wallet.createRandom().connect(ethers.provider);
+      const address = await signer.getAddress();
+      if (address < threshold) {
+        Johnny = signer;
+        Johnny_adr = address;
+        break;
+      }
+    }
+
+    return { game, Johnny, Johnny_adr };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, Johnny, Johnny_adr } = await loadFixture(
+      deployContractAndSetVariables
+    );
 
-    // good luck
+    const payer = ethers.provider.getSigner();
+    await payer.sendTransaction({
+      to: Johnny_adr,
+      value: ethers.utils.parseEther("1"),
+    });
 
-    await game.win();
+    await game.connect(Johnny).win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
